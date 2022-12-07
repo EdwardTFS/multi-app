2de4ece211f5a13f100f74e1dcdf97d5f1bda61f
Edward Görlich <goerlich_e@tfs.pl>
2022-12-08 00:23:48 +0100

    routing

--- projects/admin-app/src/app/app.module.ts
+++ projects/admin-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component,
+    Test2Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- projects/client-app/src/app/app-routing.module.ts
+++ projects/client-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'test1', component: Test1Component },
+  { path: 'test2', component: Test2Component },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
